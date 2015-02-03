97c6826b78ddc756fd7c178c5aa6970d154e6c1f
Gustavo Coutinho Cerqueira <gustavo@broadinstitute.org>
2015-02-03 14:25:00 -0500

    Update gff2graph_ort_projections_no_contraction.pl

--- gff2graph_ort_projections_no_contraction.pl
+++ gff2graph_ort_projections_no_contraction.pl
@@ -9,7 +9,7 @@ use Bio::SeqIO;
 use Bio::SeqFeature::Generic;
 use Getopt::Std;
 use GFFLib::GFFFile;
-use orthologia::Ort;
+use Orthologia::Ort;
 
 my $DEFAULT_WIDTH  = 1000;
 my $DEFAULT_HEIGHT = 600;
